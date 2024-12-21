7349435af39145f04d0ad878b525a9b389538379
Thyago Martins <58978196+ThyagoMartins0@users.noreply.github.com>
2024-12-21 05:03:43 -0300

    first commit: create folder arc and low documentation

---
+++ readme.m
@@ -0,0 +1,7 @@
+De maneira resumida esse é o backend dessa aplicação 
+O desafio aqui é usar a menor quantidade possivel de IA 
+Codar do zero tando o front quanto back
+Nesse sistema não tera nada de novo ou sistema comparado ao que trabalho atualmente 
+seguirei ao maximo as boas praticas de codigo 
+e deixarei 100% do meu codigo com coverage e testes 
+Para mais informaaçõs do projeto , acesse https://www.canva.com/design/DAGYl0b9XiM/n0soLKLG0LomWtyKeI545g/edit?utm_content=DAGYl0b9XiM&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
---
+++ src/main/java/com/ecommerce/carro/loja/de/carro/LojaDeCarroApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.carro.loja.de.carro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeCarroApplication.class, args);
+	}
+
+}
